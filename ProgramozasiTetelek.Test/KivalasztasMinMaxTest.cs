4786438e756723c2bf974e9e8bff2022d5e691f1
Poroszkai Attila <poroszkai.attila@gmail.com>
2018-10-28 10:37:11 +0100

    add selected min max item

---
+++ ProgramozasiTetelek.Test/KivalasztasMinMaxTest.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace ProgramozasiTetelek.Test
+{
+    [TestClass]
+    public class KivalasztasMinMaxTest
+    {
+        [TestMethod]
+        public void select_max_in_array_when_all_positive()
+        {
+            // Act
+            int[] arr = new int[2] { 4, 54 };
+            var select = new KivalasztasMinMax();
+            // Arrange
+            var actual = select.Max(arr);
+            // Assert
+            Assert.AreEqual(54, actual);        
+        }
+
+        [TestMethod]
+        public void select_max_in_array_when_many_positive()
+        {
+            // Act
+            var arr = new int[4] { 4, 54, 45, 13 };
+            var select = new KivalasztasMinMax();
+            // Arrange
+            var actual = select.Max(arr);
+            // Assert
+            Assert.AreEqual(54, actual);
+        }
+
+        [TestMethod]
+        public void select_max_in_array_when_some_number_negative()
+        {
+            // Act
+            var arr = new int[4] { 4, 54, 45, -13 };
+            var select = new KivalasztasMinMax();
+            // Arrange
+            var actual = select.Max(arr);
+            // Assert
+            Assert.AreEqual(54, actual);    
+        }
+
+        [TestMethod]
+        public void select_max_in_list_when_some_number_negative()
+        {
+            // Act
+            List<int> list = new List<int>() { 4, -2, 54, 0, 45, -13 };
+            var select = new KivalasztasMinMax();
+            // Arrange
+            var actual = select.Max(list);
+            // Assert
+            Assert.AreEqual(54, actual);
+        }
+
+        [TestMethod]
+        public void select_max_in_list_when_all_number_negative()
+        {
+            // Act
+            List<int> list = new List<int>() { -4, -2, -54, -45, -13 };
+            var select = new KivalasztasMinMax();
+            // Arrange
+            var actual = select.Max(list);
+            // Assert
+            Assert.AreEqual(-2, actual);
+        }
+
+        [TestMethod]
+        public void select_max_in_array_when_all_number_negative_and_one_null_value()
+        {
+            // Act
+            var arr = new int[6] { -4, -2, -54, 0, -45, -13 };
+            var select = new KivalasztasMinMax();
+            // Arrange
+            var actual = select.Max(arr);
+            // Assert
+            Assert.AreEqual(0, actual);
+        }
+    }
+}
---
+++ ProgramozasiTetelek/KivalasztasMInMax.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgramozasiTetelek
+{
+    /// <summary>
+    /// Egy „n” elemű sorozatban (tömb, lista…) azon elem sorszámának és értékének a meghatározása, amelyik a
+    /// legnagyobb/legkisebb elem az adott sorozatban.
+    /// </summary>
+    public class KivalasztasMinMax
+    {
+        /// <summary>
+        /// Kiválasztja a tömbből a legnagyobb értékű számot
+        /// </summary>
+        /// <param name="Array"></param>
+        /// <returns>A legnagyobb szám a tömbben</returns>
+        public int Max(int[] Array)
+        {
+            int eredmeny = 0;
+
+            // Elmentjük a sorozat első tagját és ehez fogjuk mérni a többit
+            int pointer = Array[0];
+
+            // bejárom a tömböt egy for ciklussal
+            for (int i = 0; i < Array.Length; i++)
+            {
+                // Ha a pointer értéke kisebb mint a cikluselem ahol éppen tartok         
+                if (pointer < Array[i])
+                {                       
+                    // akkor átírom az eredménybe az értéket, mivel ez az eddigi legnagyobb amivel a tömb
+                    // bejárása során találkoztam 
+                    eredmeny = Array[i];
+
+                    // a pointert is átállítom erre az értékre, hogy a tömb további elemeit
+                    // ehez az értékhez hasonlítsa
+                    pointer = Array[i];
+                }
+            }
+
+            return eredmeny;
+        }
+
+        /// <summary>
+        /// kiválasztja a listából a legnagyobb értékű számot
+        /// </summary>
+        /// <param name="list"></param>
+        /// <returns>A legnagyobb szám a listában</returns>
+        public int Max(List<int> list)
+        {
+            // egy nyelvi környezet adta átalakítása az integerekkel teli lista
+            // integerekkel teli tömbbé, sokkal egyszerűbben, mint a Osszegzes tételénél
+            int[] arr = list.ToArray();
+            return Max(arr);
+        }
+    }
+}
