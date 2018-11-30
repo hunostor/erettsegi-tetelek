da82feaa60069c580d3f29a4f56c1ceecbf03092
Poroszkai Attila <poroszkai.attila@gmail.com>
2018-11-30 10:55:29 +0100

    buborékos rendezés

---
+++ ProgramozasiTetelek.Test/BuborekosRendezesTeszt.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgramozasiTetelek.Test
+{
+    [TestClass]
+    public class BuborekosRendezesTeszt
+    {        
+        [TestMethod]
+        public void RendezesTeszteles()
+        {
+            // Act
+            List<int> testList = new List<int>()
+            {
+                32, 12, 10, 9, 45, 90, 13, 8, 1, 42
+            };
+            BuborekosRendezes rendezes = new BuborekosRendezes();
+            List<int> actualList;
+            // Arrange
+            actualList = rendezes.Rendezes(testList);
+            // Assert
+            Assert.AreEqual(1, actualList[0]);
+            Assert.AreEqual(90, actualList[9]);
+            Assert.AreEqual(45, actualList[8]);
+            Assert.AreEqual(8, actualList[1]);
+            Assert.AreEqual(9, actualList[2]);
+            Assert.AreEqual(13, actualList[5]);
+        }
+
+        [TestMethod]
+        public void RendezesTeszteles_vannak_listaban_negativ_szamok_is()
+        {
+            // Act
+            List<int> testList = new List<int>()
+            {
+                32, 12, 10, -3, 9, 45, 90, 13, -444, 8, 1, 42
+            };
+            BuborekosRendezes rendezes = new BuborekosRendezes();
+            List<int> actualList;
+            // Arrange
+            actualList = rendezes.Rendezes(testList);
+            // Assert
+            Assert.AreEqual(-444, actualList[0]);
+            Assert.AreEqual(-3, actualList[1]);
+            Assert.AreEqual(90, actualList[11]);
+        }
+    }
+}
---
+++ ProgramozasiTetelek/BuborekosRendezes.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgramozasiTetelek
+{
+    /// <summary>
+    /// Végigmegy egy sorozaton (tömb, lista…), és ha a szomszédos elemeknél rossz a sorrend, akkor megcseréli őket. (ez a
+    /// csere, mint egy buborék végighalad a tömbön, és a legnagyobb elem, biztosan a tömb végére kerül
+    /// </summary>
+    public class BuborekosRendezes
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="rendezetlenLista"></param>
+        /// <returns></returns>
+        public List<int> Rendezes(List<int> rendezetlenLista)
+        {
+            List<int> rendezett;
+            // Lefele szamoltatjuk a ciklus pointert
+            for (int i = rendezetlenLista.Count -1; i > 1; i--)
+            {
+                // Ezért volt értelme az i -t lefelé számoltatni
+                // most lehet olyan feltetelünk, hogy a addig fusson míg a j kisebb mint
+                // az i
+                for (int j = 0; j < i; j++)
+                {
+                    // Itt képződik a "buborék" ha az utánna következő érték nagyobb a sorban
+                    // akkor elkezdődik a vándorlás, az elemek megcserélődnek
+                    if(rendezetlenLista[j] > rendezetlenLista[j + 1])
+                    {
+                        int atmenetiTarolo = rendezetlenLista[j];
+                        rendezetlenLista[j] = rendezetlenLista[j + 1];
+                        rendezetlenLista[j + 1] = atmenetiTarolo;
+                    }
+                }
+            }
+
+            rendezett = rendezetlenLista;
+            return rendezett;
+        }
+    }
+}
